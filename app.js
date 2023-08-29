4f681335a10f8b191e5a01af986a62879f869dcb
Avik Mitra <avikmitra420@gmail.com>
2023-08-29 14:12:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,16 +11,17 @@ const db = process.env.URI || uri
 var cors = require("cors");
 /* app.set('view engine', 'ejs')
 app.set('views', './views') */
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-type",
-    "Authorization"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-type",
+//     "Authorization"
+//   );
+//   next();
+// });
+app.use(cors());
 app.use("", routes);
 app.use(express.json());
 
